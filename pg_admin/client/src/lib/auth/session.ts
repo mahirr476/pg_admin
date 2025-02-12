4077c6ca65a91f56a8360b83ebf172f2167bdcd6
mahirr476 <rmahirr476@gmail.com>
2025-02-12 15:50:41 +0600

    removed old proj structure, now only client structure made by Joha

--- pg_admin/client/src/lib/auth/session.ts
+++
@@ -1,9 +0,0 @@
-// src/lib/auth/session.ts
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "./config";
-import { Session } from "@/types/user";
-
-export async function getCurrentUser() {
-  const session = await getServerSession(authOptions) as Session | null;
-  return session?.user;
-}
