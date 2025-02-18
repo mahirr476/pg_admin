75a6a883aea3be9f984a1638a8e49fedce0654dc
Samsujjoha <samsujjoha99@gmail.com>
2025-02-18 12:04:17 +0600

    design done by buisness page

--- pg_admin/client/src/app/admin/paragon/buisness/page.tsx
+++ pg_admin/client/src/app/admin/paragon/buisness/page.tsx
@@ -272,3 +272,20 @@ export default function ParagonBusinessPage() {
     </div>
   );
 }
+
+
+
+// import BusinessModal from '@/components/paragon/buisness/buisness1'
+// import BusinessModal1 from '@/components/paragon/buisness/buisness2'
+// import React from 'react'
+
+// const Buisness = () => {
+//   return (
+//     <div>
+//       <BusinessModal/>
+//       <BusinessModal1/>
+//     </div>
+//   )
+// }
+
+// export default Buisness
