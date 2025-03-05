078d664ddb8e475ccad914f0804c7cf3f589dbda
pitash <pitashkumar03@gmail.com>
2025-03-05 12:50:01 +0600

    Implement in role routes for role-based permissions

--- pg_admin/server/src/routes/global/auth.routes.ts
+++ pg_admin/server/src/routes/global/auth.routes.ts
@@ -11,6 +11,6 @@ router.post('/create', authMiddleware, authorize(['user_create']), createUserHan
 router.get('/all', authMiddleware, authorize(['user_view']), getAllUsersHandler);
 router.get('/:id', authMiddleware, authorize(['user_view']), getUserByIdHandler);
 router.put('/:id', authMiddleware, authorize(['user_edit']), updateUserHandler);
-router.get('/inactive-users', authMiddleware, authorize(['user_view']), getInactiveUsersHandler);
+router.get('/inactive-users', authMiddleware, authorize(['user_edit']), getInactiveUsersHandler);
 
 export default router;
