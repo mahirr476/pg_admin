54e154599cd26c50c2e27d637259c0413b37f026
pitash <pitashkumar03@gmail.com>
2025-03-09 10:32:31 +0600

    Make another response format for audit log

--- pg_admin/server/src/controllers/global/audit-log.controller.ts
+++ pg_admin/server/src/controllers/global/audit-log.controller.ts
@@ -9,7 +9,22 @@ export const AuditController = {
           res.status(200).json({ 
               status: "success",
               message: 'AuditLog fetched successfully', 
-              auditLogs 
+              // auditLogs
+              auditLogs: auditLogs.map((auditLog:any) => ({
+                id: auditLog.id,
+                userName: auditLog.userName,
+                userEmail: auditLog.userEmail,
+                action: auditLog.action,
+                error_message: auditLog.error_message,
+                formattedDate: auditLog.formattedDate,
+                ip_address: auditLog.ip_address,
+                entity_type: auditLog.entity_type,
+                entity_id: auditLog.entity_id,
+                previous_state: auditLog.previous_state,
+                new_state: auditLog.new_state,
+                user_agent: auditLog.user_agent,
+                notes: auditLog.notes,
+              })),
           });
       } catch (error) {
           // console.error('Error fetch auditLogs:', error);
