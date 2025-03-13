5203b46738792e56a6cc86e7d29b79f646fd33f9
pitash <pitashkumar03@gmail.com>
2025-03-13 13:29:22 +0600

    modified some code

--- pg_admin/server/src/routes/group/hero.routes.ts
+++ pg_admin/server/src/routes/group/hero.routes.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 import { HeroController } from '../../controllers/group/hero.controller';
 import { authMiddleware } from '../../middleware/auth.middleware';
+import { authorize } from '../../middleware/authorization.middleware';
 
 const router = express.Router();
 
 // Create a new hero
 // router.post("/hero", authMiddleware, HeroController.create);
 
-router.post("/hero", authMiddleware, HeroController.createOrUpdate);
+router.post("/hero", authMiddleware, authorize(['paragon_group_create']), HeroController.createOrUpdate);
 
 // Get all heroes
-router.get('/hero', authMiddleware, HeroController.getAll);
+router.get('/hero', authMiddleware, authorize(['paragon_group_view']), HeroController.getAll);
 
 export default router;
