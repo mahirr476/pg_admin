c5222570caf25b70946c49963cfa21f933a9b3d6
Samsujjoha <samsujjoha99@gmail.com>
2025-04-03 09:45:54 +0600

    solved error in certificate page

---
+++ pg_admin/client/src/app/admin/paragon/business/certificate/page.tsx
@@ -0,0 +1,12 @@
+import BusinessCertificationPage from '@/components/paragon/buisness-activity/certificates/page'
+import React from 'react'
+
+const Certificate = () => {
+  return (
+    <div>
+        <BusinessCertificationPage/>
+    </div>
+  )
+}
+
+export default Certificate
