c612d876d9c2b6778db31ca20024b3d4469d3646
Samsujjoha <samsujjoha99@gmail.com>
2025-04-07 09:18:29 +0600

    added loading for paragon and parasole

---
+++ pg_admin/client/src/app/admin/parasole/loading.tsx
@@ -0,0 +1,12 @@
+import Loading from '@/components/loading/page'
+import React from 'react'
+
+const LoadingParasole = () => {
+  return (
+    <div>
+        <Loading/>
+    </div>
+  )
+}
+
+export default LoadingParasole
