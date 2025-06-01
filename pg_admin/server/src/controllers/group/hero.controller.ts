4078455b7a01916f89556b3b49c92f98e51b6242
pitash <pitashkumar03@gmail.com>
2025-06-01 12:33:41 +0600

    automated github code deployment test

--- pg_admin/server/src/controllers/group/hero.controller.ts
+++ pg_admin/server/src/controllers/group/hero.controller.ts
@@ -141,7 +141,7 @@ export const HeroController = {
             
             res.status(200).json({
                 success: true,
-                message: "Heroes1 retrieved successfully",
+                message: "Heroes0 retrieved successfully",
                 data: heroes.map(hero => ({
                     ...hero,
                     createdAt: formatDate(hero.createdAt),
