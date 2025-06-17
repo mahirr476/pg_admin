7d4b2f90c4b4d22fbcef02abd59743677ccd5d33
Samsujjoha <samsujjoha99@gmail.com>
2025-06-17 16:04:28 +0600

    solve image issue in milestone page

--- pg_admin/client/src/components/paragon/milestones/milestones-main.tsx
+++ pg_admin/client/src/components/paragon/milestones/milestones-main.tsx
@@ -773,7 +773,11 @@ const MainMilestones: React.FC = () => {
                         <div className="h-16 w-16 rounded-lg overflow-hidden bg-gray-100 flex items-center justify-center relative border border-gray-200 shadow-sm">
                           {milestone.image ? (
                             <img
-                              src={`http://localhost:7000/${img}`}
+                            src={`http://localhost:7000/${milestone.image}`}
+                              // src={`http://api.pg-admin.57.155.183.218.nip.io/${milestone.image.replace(
+                              //   /^public\//,
+                              //   ""
+                              // )}`}
                               alt={milestone.title}
                               className="h-full w-full object-cover"
                               onError={(e) => {
