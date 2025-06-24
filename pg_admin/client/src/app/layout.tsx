fa9a3c11169ff311e21c66cf77c6ef02af11d578
Samsujjoha <samsujjoha99@gmail.com>
2025-06-24 17:25:09 +0600

    trying to maintain same layout

--- pg_admin/client/src/app/layout.tsx
+++ pg_admin/client/src/app/layout.tsx
@@ -7,7 +7,6 @@ import { WebsiteProvider } from '@/providers/WebsiteProvider'
 import { AuthProvider } from '@/providers/auth-provider'
 import { PermissionProvider } from '@/providers/permission-context'
 import { ClientLayout } from '@/components/layout/ClientLayout'
-import Loading from "@/components/loading/page"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -24,11 +23,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <Loading/>
         <AuthProvider>
           <PermissionProvider>
             <WebsiteProvider>
