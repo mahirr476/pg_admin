e8c7dcf5f47b1d637d7e86800a142d41ba808c25
Samsujjoha <samsujjoha99@gmail.com>
2025-06-24 17:19:50 +0600

    trying to maintain same layout

--- pg_admin/client/src/app/layout.tsx
+++ pg_admin/client/src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
